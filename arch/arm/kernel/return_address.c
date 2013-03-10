6e47a0557c3a93091defeef47463068f31969c87
Dennis Rassmann <showp1984@gmail.com>
2013-03-10 07:33:21 +0100

    kernel/return_address.c: hide warning
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    
    Conflicts:
    	arch/arm/kernel/return_address.c

--- arch/arm/kernel/return_address.c
+++ arch/arm/kernel/return_address.c
@@ -60,6 +60,8 @@ void *return_address(unsigned int level)
 
 #if defined(CONFIG_ARM_UNWIND)
 /* #warning "TODO: return_address should use unwind tables" */
+#if 0
+#warning "TODO: return_address should use unwind tables"
 #endif
 
 #endif /* if defined(CONFIG_FRAME_POINTER) && !defined(CONFIG_ARM_UNWIND) / else */
